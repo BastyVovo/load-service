3265ec50df52d7e57c5e7525f5e2c02b4c766a51
basty <bastyaccnt.0@gmail.com>
2025-11-16 13:11:18 +0800

    update ui

--- src/script/index.js
+++ src/script/index.js
@@ -47,7 +47,7 @@ document.getElementById('loadForm').addEventListener('submit', async function(e)
   }
 
   if (selectedAmount < 100 || selectedAmount > 1000) {
-    showStatus('error', 'Please select or enter an amount (minimum ₱100 and maximum ₱1000');
+    showStatus('error', 'Please select or enter an amount (minimum ₱100 and maximum ₱1000)');
     return;
   }
 
